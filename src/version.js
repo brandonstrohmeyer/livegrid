e81ea339ed882d7576ea51df5810df9a570e8ca2
Brandon Strohmeyer <brandon@stro.io>
2026-03-20 12:32:31 -0400

    fix build

--- src/version.js
+++ src/version.js
@@ -1,3 +1,7 @@
-// vite-plugin-static-copy or import.meta.glob can be used for more advanced cases
-import versionData from '../build.json'
-export default versionData.version
+const buildFiles = import.meta.glob('../build.json', { eager: true, import: 'default' })
+const packageFiles = import.meta.glob('../package.json', { eager: true, import: 'default' })
+
+const buildVersion = buildFiles['../build.json']?.version
+const packageVersion = packageFiles['../package.json']?.version
+
+export default buildVersion || packageVersion || '0.0.0'
